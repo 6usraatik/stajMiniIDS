6c2ae8d84482451eadd11d8733c8249fb11c149d
6usraatik <6usraatik@gmail.com>
2026-07-08 09:59:56 +0300

    IPv4 internet trafiği ayrıştırma motoru eklendi, çoklu protokol analizi aktif hale getirildi.

--- stajProje.cpp
+++ stajProje.cpp
@@ -1,55 +1,106 @@
 #include <iostream>
 #include <pcap.h>
-#pragma comment(lib, "ws2_32.lib") //LNK2019 hatasýný almamak için
+#include <map>
+#include <string>
+#pragma comment(lib, "ws2_32.lib") // LNK2019 hatasý almamak için
 
-// 14 bayt
+// ethernet baþlýðý
 struct ethernet_header {
-    u_char hedef_mac[6]; //6 bayt
-    u_char kaynak_mac[6]; //6 bayt
-    u_short eth_type; //2 bayt
+    u_char hedef_mac[6];
+    u_char kaynak_mac[6];
+    u_short eth_type;
 };
 
-// 28 bayt
+// arp baþlýðý
 struct arp_header {
-    u_short donanim_tipi; // ethernet için her zaman 1 
-    u_short protokol_tipi;// IPv4 için 0x0800 
-    u_char  donanim_len;  // MAC adresi uzunluðu (6 bayt)
-    u_char  protokol_len; // IP adresi uzunluðu (4 bayt)
-    u_short islem_tipi;   // 1 = ARP request (soru), 2 = ARP reply (cevap)
-    u_char  gonderen_mac[6]; // gönderen cihazýn MAC'i
-    u_char  gonderen_ip[4];  // gönderen cihazýn IP'si 
-    u_char  hedef_mac[6];    // aranan cihazýn MAC'i
-    u_char  hedef_ip[4];     // aranan cihazýn IP'si 
+    u_short donanim_tipi;
+    u_short protokol_tipi;
+    u_char  donanim_len;
+    u_char  protokol_len;
+    u_short islem_tipi;
+    u_char  gonderen_mac[6];
+    u_char  gonderen_ip[4];
+    u_char  hedef_mac[6];
+    u_char  hedef_ip[4];
 };
 
-void mac_yazdir(const u_char* mac) {
-    printf("%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+// ipv4 baþlýðý
+struct ip_header {
+    u_char  ver_ihl;        // versiyon (4 bit) + baþlýk uzunluðu (4 bit)
+    u_char  tos;            // hizmet tipi
+    u_short tlen;           // toplam uzunluk
+    u_short id;             // kimlik
+    u_short flags_fo;       // bayraklar + fragman offseti
+    u_char  ttl;            // yaþam süresi (time to live)
+    u_char  proto;          // üst katman protokolü (TCP=6, UDP=17, ICMP=1)
+    u_short crc;            // baþlýk saðlama toplamý
+    u_char  kaynak_ip[4];   // gönderen cihazýn ip'si
+    u_char  hedef_ip[4];    // hedef cihazýn ip'si
+};
+
+// güvenlik hafýzasý
+std::map<std::string, std::string> ip_mac_tablosu;
+
+// yard. fonk.
+std::string ip_to_string(const u_char* ip) {
+    return std::to_string(ip[0]) + "." + std::to_string(ip[1]) + "." +
+        std::to_string(ip[2]) + "." + std::to_string(ip[3]);
 }
 
-// IP adresini basan fonksiyon
-void ip_yazdir(const u_char* ip) {
-    printf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+std::string mac_to_string(const u_char* mac) {
+    char buffer[18];
+    sprintf_s(buffer, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return std::string(buffer);
 }
+
 
 void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_char* pkt_data) {
-    
     const struct ethernet_header* eth = (struct ethernet_header*)pkt_data;
-    u_short protokol = ntohs(eth->eth_type); //network to host short
+    u_short protokol = ntohs(eth->eth_type);
 
-    // sadece ARP paketleri
+    // eðer paket arp ise
     if (protokol == 0x0806) {
-        static int arp_sayisi = 1;
-        std::cout << "\n[" << arp_sayisi++ << ". ARP PAKETÝ YAKALANDI!]\n";
+        const struct arp_header* arp = (struct arp_header*)(pkt_data + 14);
+        std::string gonderen_ip = ip_to_string(arp->gonderen_ip);
+        std::string gonderen_mac = mac_to_string(arp->gonderen_mac);
+
+        if (ip_mac_tablosu.find(gonderen_ip) == ip_mac_tablosu.end()) {
+            ip_mac_tablosu[gonderen_ip] = gonderen_mac;
+            std::cout << "\n[+] [ARP BELLEK] YENI CIHAZ ADRESI GUVENLE KAYDEDILDI -> IP: " << gonderen_ip << " | MAC: " << gonderen_mac << std::endl;
+        }
+        else {
+            std::string bilinen_mac = ip_mac_tablosu[gonderen_ip];
+            if (bilinen_mac != gonderen_mac) {
+                std::cout << "\n=============================================================\n";
+                std::cout << " KRITIK GUVENLIK ALARMI: ARP SPOOFING / MITM TESPITI!\n";
+                std::cout << "=============================================================\n";
+                std::cout << " -> Sahtekarlik Yapilan IP Adresi : " << gonderen_ip << std::endl;
+                std::cout << " -> Hafizadaki Gercek MAC Adresi  : " << bilinen_mac << std::endl;
+                std::cout << " -> Saldirgandan Gelen Sahte MAC  : " << gonderen_mac << std::endl;
+                std::cout << "=============================================================\n";
+            }
+        }
+    }
+
+    // eðer paket ip ise
+    else if (protokol == 0x0800) {
+        static int ip_paket_sayisi = 1;
 
         
-        const struct arp_header* arp = (struct arp_header*)(pkt_data + 14);
+        const struct ip_header* ip = (struct ip_header*)(pkt_data + 14);
 
-        u_short islem = ntohs(arp->islem_tipi);
-        if (islem == 1) std::cout << " [Tur] : ARP SORUSU (Who has...?) -> Bir IP'nin MAC'i araniyor.\n";
-        else if (islem == 2) std::cout << " [Tur] : ARP CEVABI (Reply) -> MAC adresi bildiriliyor.\n";
+        std::cout << "\n[" << ip_paket_sayisi++ << ". IPv4 Internet Paketi] - Boyut: " << header->len << " bayt\n";
+        std::cout << "   Kaynak IP -> " << ip_to_string(ip->kaynak_ip) << std::endl;
+        std::cout << "   Hedef IP  -> " << ip_to_string(ip->hedef_ip) << std::endl;
 
-        std::cout << " Gönderen : IP ("; ip_yazdir(arp->gonderen_ip); std::cout << ") - MAC ("; mac_yazdir(arp->gonderen_mac); std::cout << ")\n";
-        std::cout << " Aranan   : IP ("; ip_yazdir(arp->hedef_ip); std::cout << ") - MAC ("; mac_yazdir(arp->hedef_mac); std::cout << ")\n";
+        // içerideki taþýnan protokolün tespiti
+        std::cout << "   Tasinan Protokol: ";
+        if (ip->proto == 1)       std::cout << "ICMP (Ping Sinyali)" << std::endl;
+        else if (ip->proto == 6)  std::cout << "TCP (Web / Guvenli Veri Trafigi)" << std::endl;
+        else if (ip->proto == 17) std::cout << "UDP (Canli Yayin / DNS Sorgusu)" << std::endl;
+        else                      std::cout << "Diger (" << (int)ip->proto << ")" << std::endl;
+
+        std::cout << "   Yasam Suresi (TTL): " << (int)ip->ttl << " hop\n";
         std::cout << "-------------------------------------------------------------";
     }
 }
@@ -60,7 +111,6 @@ int main() {
     int i = 0, secim;
 
     if (pcap_findalldevs(&alldevs, errbuf) == -1) return 1;
-
     for (d = alldevs; d != NULL; d = d->next) {
         std::cout << ++i << ". " << d->name << " (" << (d->description ? d->description : "Tanim yok") << ")\n";
     }
@@ -70,15 +120,13 @@ int main() {
     std::cin >> secim;
 
     for (d = alldevs, i = 0; i < secim - 1; d = d->next, i++);
-    std::cout << "\n[" << d->description << "] uzerinden ARP paketleri bekleniyor...\n";
+    std::cout << "\n[" << d->description << "] uzerinden Akilli Paket Analizi (IDS) Aktif...\n";
 
     pcap_t* adhandle = pcap_open_live(d->name, 65536, 1, 1000, errbuf);
     if (adhandle == NULL) return -1;
     pcap_freealldevs(alldevs);
 
-    // zorla durdurulana kadar dinle
     pcap_loop(adhandle, 0, packet_handler, NULL);
-
     pcap_close(adhandle);
     return 0;
 }
