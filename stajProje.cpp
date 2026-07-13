ad769c4f6375f48796dedec1ae1bbee52a381e48
6usraatik <6usraatik@gmail.com>
2026-07-13 10:08:38 +0300

    Davranissal anomali tespiti eklandi: Nmap Scan algilama motoru ve Blacklist altyapisi kuruldu.

--- stajProje.cpp
+++ stajProje.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <pcap.h>
 #include <map>
+#include <set>     
 #include <string>
 #include <fstream>
 #pragma comment(lib, "ws2_32.lib")
 
-// istatistik sayacý
+
 int toplam_paket = 0;
 int tcp_sayisi = 0;
 int udp_sayisi = 0;
 int arp_sayisi = 0;
 int alarm_sayisi = 0;
+int port_tarama_sayisi = 0; 
+int kara_liste_engeli = 0;  
 
 
 struct ethernet_header { u_char hedef_mac[6]; u_char kaynak_mac[6]; u_short eth_type; };
@@ -28,9 +31,14 @@ struct ip_header {
 struct tcp_header { u_short kaynak_port; u_short hedef_port; };
 struct udp_header { u_short kaynak_port; u_short hedef_port; };
 
-std::map<std::string, std::string> ip_mac_tablosu;
 
-// log dosyasý
+std::map<std::string, std::string> ip_mac_tablosu;             
+std::set<std::string> kara_liste;                             
+std::map<std::string, std::set<u_short>> port_tarama_takibi;  
+
+// port taramasý alarmý için eþik deðeri (kolay olsun diye 4)
+const int PORT_TARAMA_LIMITI = 4;
+
 std::ofstream log_dosyasi("C:\\Users\\HUAWEI\\Desktop\\ids_guvenlik_log.txt", std::ios::app);
 
 std::string ip_to_string(const u_char* ip) {
@@ -44,13 +52,13 @@ std::string mac_to_string(const u_char* mac) {
 
 
 void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_char* pkt_data) {
-    toplam_paket++; // her gelen pakette sayacý artýr
+    toplam_paket++;
     const struct ethernet_header* eth = (struct ethernet_header*)pkt_data;
     u_short protokol = ntohs(eth->eth_type);
 
-    // arp
+    
     if (protokol == 0x0806) {
-        arp_sayisi++; 
+        arp_sayisi++;
         const struct arp_header* arp = (struct arp_header*)(pkt_data + 14);
         std::string gonderen_ip = ip_to_string(arp->gonderen_ip);
         std::string gonderen_mac = mac_to_string(arp->gonderen_mac);
@@ -60,17 +68,21 @@ void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_cha
         }
         else {
             std::string bilinen_mac = ip_mac_tablosu[gonderen_ip];
-            // test için baþta == yapýldý sonra olmasý gereken != formatýna çevrildi
-            if (bilinen_mac != gonderen_mac) {
-                alarm_sayisi++; 
-                std::cout << "\nKRITIK GUVENLIK ALARMI: ARP SPOOFING! -> IP: " << gonderen_ip << "\n";
+            if (bilinen_mac != gonderen_mac) { // test için baþta == yapýldý sonradan != þekline çevrildi
+                alarm_sayisi++;
+                std::cout << "\nKRITIK ALARM: ARP SPOOFING! -> IP: " << gonderen_ip << "\n";
+
+                // kara listeye alma
+                kara_liste.insert(gonderen_ip);
+                std::cout << "[+] " << gonderen_ip << " KARA LISTEYE EKLENDI!\n";
+
                 if (log_dosyasi.is_open()) {
-                    log_dosyasi << "[ALARM] ARP Spoofing! IP: " << gonderen_ip << " | MAC: " << gonderen_mac << "\n";
+                    log_dosyasi << "[ALARM - ARP] Sahte MAC! IP: " << gonderen_ip << " Kara Listeye Alindi.\n";
                 }
             }
         }
     }
-    // ipv4
+    
     else if (protokol == 0x0800) {
         const struct ip_header* ip = (struct ip_header*)(pkt_data + 14);
         int ip_baslik_uzunlugu = (ip->ver_ihl & 0x0F) * 4;
@@ -78,19 +90,57 @@ void packet_handler(u_char* param, const struct pcap_pkthdr* header, const u_cha
         std::string s_ip = ip_to_string(ip->kaynak_ip);
         std::string h_ip = ip_to_string(ip->hedef_ip);
 
+        // ip kara listede mi
+        if (kara_liste.find(s_ip) != kara_liste.end()) {
+            kara_liste_engeli++;
+            std::cout << "[KARA LISTE ENGELLI TRAFIK] Kaynak: " << s_ip << " -> Hedef: " << h_ip << "\n";
+            if (log_dosyasi.is_open()) {
+                log_dosyasi << "[KARA LISTE TRAFIGI] " << s_ip << " sistemle iletisim kurmaya calisti.\n";
+            }
+            return; // kara listedeki adresi atla
+        }
+
+        u_short s_port = 0, h_port = 0;
+
         if (ip->proto == 6) {
-            tcp_sayisi++; 
+            tcp_sayisi++;
             const struct tcp_header* tcp = (struct tcp_header*)(pkt_data + 14 + ip_baslik_uzunlugu);
-            u_short s_port = ntohs(tcp->kaynak_port);
-            u_short h_port = ntohs(tcp->hedef_port);
+            s_port = ntohs(tcp->kaynak_port);
+            h_port = ntohs(tcp->hedef_port);
             std::cout << "[TCP] " << s_ip << ":" << s_port << " --> " << h_ip << ":" << h_port << "\n";
         }
         else if (ip->proto == 17) {
-            udp_sayisi++; 
+            udp_sayisi++;
             const struct udp_header* udp = (struct udp_header*)(pkt_data + 14 + ip_baslik_uzunlugu);
-            u_short s_port = ntohs(udp->kaynak_port);
-            u_short h_port = ntohs(udp->hedef_port);
+            s_port = ntohs(udp->kaynak_port);
+            h_port = ntohs(udp->hedef_port);
             std::cout << "[UDP] " << s_ip << ":" << s_port << " --> " << h_ip << ":" << h_port << "\n";
+        }
+
+        // anomali tespiti
+        if (h_port != 0) {
+            
+            port_tarama_takibi[s_ip].insert(h_port);
+
+            // eþik deðer aþýldýysa alarm ver
+            if (port_tarama_takibi[s_ip].size() == PORT_TARAMA_LIMITI) {
+                alarm_sayisi++;
+                port_tarama_sayisi++;
+                std::cout << "\n=============================================================\n";
+                std::cout << " KRITIK GUVENLIK ALARMI: PORT TARAMASI (NMAP) TESPITI!\n";
+                std::cout << "=============================================================\n";
+                std::cout << " -> Saldirgan IP : " << s_ip << "\n";
+                std::cout << " -> Durum        : Kisa surede " << PORT_TARAMA_LIMITI << " farkli portu yokladi!\n";
+                std::cout << " -> Aksiyon      : IP Adresi KARA LÝSTEYE alindi.\n";
+                std::cout << "=============================================================\n";
+
+                // kara listeye alma
+                kara_liste.insert(s_ip);
+
+                if (log_dosyasi.is_open()) {
+                    log_dosyasi << "[ALARM - PORT SCAN] " << s_ip << " port taramasi yapti! Kara listeye eklendi.\n";
+                }
+            }
         }
     }
 }
@@ -113,41 +163,34 @@ int main() {
     if (adhandle == NULL) return -1;
     pcap_freealldevs(alldevs);
 
-    // bpf filtresi
-    std::cout << "\n[BPF] Uygulanacak filtreyi yazin (Orn: tcp, udp, arp) veya tumu icin bos birakin: ";
+    std::cout << "\n[BPF] Uygulanacak filtreyi yazin (Tumu icin bos birakin): ";
     std::cin.ignore();
     std::getline(std::cin, filtre_girdisi);
 
     if (!filtre_girdisi.empty()) {
         struct bpf_program fcode;
-        
-        if (pcap_compile(adhandle, &fcode, filtre_girdisi.c_str(), 1, PCAP_NETMASK_UNKNOWN) < 0) {
-            std::cout << "\n[HATA] Hatali filtre soz dizimi!\n";
-            return -1;
+        if (pcap_compile(adhandle, &fcode, filtre_girdisi.c_str(), 1, PCAP_NETMASK_UNKNOWN) >= 0) {
+            pcap_setfilter(adhandle, &fcode);
+            std::cout << "[+] BPF Filtresi Aktif: " << filtre_girdisi << "\n";
         }
-        pcap_setfilter(adhandle, &fcode);
-        std::cout << "[+] BPF Filtresi Aktif: " << filtre_girdisi << "\n";
     }
 
-    std::cout << "Kac paket yakalandiktan sonra oturum sonlansin? (Orn: 50, 1000): ";
+    std::cout << "Kac paket yakalandiktan sonra oturum sonlansin?: ";
     std::cin >> paket_limiti;
 
-    std::cout << "\nAg dinleniyor... Bitis limiti: " << paket_limiti << " paket.\n";
-
-    
+    std::cout << "\nAg dinleniyor... (Port Taramasý Korumasý ve Kara Liste Aktif)\n";
     pcap_loop(adhandle, paket_limiti, packet_handler, NULL);
     pcap_close(adhandle);
     if (log_dosyasi.is_open()) log_dosyasi.close();
 
-    // istatistik raporu
+    
     std::cout << "\n================ OTURUM OZETI ================\n";
     std::cout << " Toplam Incelenen Paket  : " << toplam_paket << "\n";
-    std::cout << " Analiz Edilen TCP       : " << tcp_sayisi << "\n";
-    std::cout << " Analiz Edilen UDP       : " << udp_sayisi << "\n";
-    std::cout << " Yakalanan ARP           : " << arp_sayisi << "\n";
-    std::cout << " Uretilen Guvenlik Alarmi: " << alarm_sayisi << "\n";
+    std::cout << " Analiz Edilen TCP/UDP   : " << tcp_sayisi << " / " << udp_sayisi << "\n";
+    std::cout << " Tespit Edilen Port Scan : " << port_tarama_sayisi << "\n";
+    std::cout << " Toplam Guvenlik Alarmi  : " << alarm_sayisi << "\n";
+    std::cout << " Kara Listeden Engellenen: " << kara_liste_engeli << " paket\n";
     std::cout << "==============================================\n";
-    std::cout << "Log dosyasi Masaustune kaydedildi.\n";
 
     std::cout << "Cikmak icin bir tusa basin...";
     std::cin.ignore();
